5bc2cc17e1f90944acb33e2f1d58e60ddda2ded7
Mark Pichler <mark_pichler@yahoo.com>
2018-12-03 14:27:03 -0800

    Add valley finding function

--- rsi-divergence/binance_drivers.py
+++ rsi-divergence/binance_drivers.py
@@ -1,15 +1,37 @@
 import json, requests
 
 def price_history(interval, symbol):
+   
     price_array = []
-    payload = {"interval": interval, "symbol": symbol, "limit": 100}
+    payload = {"interval": interval, "symbol": symbol, "limit": 30}
 
     r = requests.get("https://api.binance.com/api/v1/klines", params=payload)
  
     for c in json.loads(r.text):
         price_array.append(c[4])
-       
+    
+    
     return price_array
+
+
+
+def find_lows(data):
+    lows = []
+    for i in range(len(data)):
+        # Assumes data array len is greater than 1
+        if i == 0 and data[i] <= data[i + 1]:
+            lows.append(data[i]) 
+        elif i == len(data) - 1 and data[i] <= data[i - 1]:
+            lows.append(data[i])
+        elif data[i - 1] >= data[i] <= data[i + 1]:
+            lows.append(data[i])
+        else:
+            lows.append(-1)
+
+    return lows
+
+
+
 
 """def calculate_rsi(interval, period, symbol):
     init_avg_gain = 0
@@ -21,3 +43,4 @@ def price_history(interval, symbol):
     for i in range(len(candles)):
         print("1")
 """
+print(find_lows(price_history("15m", "BTCUSDT")))
