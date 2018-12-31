20feed62cced34a76d99a233f30db2c1511a3509
Mark Pichler <mark_pichler@yahoo.com>
2018-12-30 23:37:52 -0800

    Add method to update price array with most recent close

--- rsi-divergence/binance_drivers.py
+++ rsi-divergence/binance_drivers.py
@@ -13,6 +13,8 @@ class Asset:
     def __init__(self, interval, symbol):
         """Collect initial short term price and RSI history."""
 
+        self.interval = interval
+        self.symbol = symbol
         self.rsi = []
         self.prices = []
         self.prev_gain_ema = 0
@@ -49,21 +51,34 @@ class Asset:
                 u = 0
                 d = -change
 
-            cur_gain_ema = a * u + (1 - a) * self.prev_gain_ema
-            cur_loss_ema = a * d + (1 - a) * self.prev_loss_ema
+            cur_gain_ema = self.a * u + (1 - self.a) * self.prev_gain_ema
+            cur_loss_ema = self.a * d + (1 - self.a) * self.prev_loss_ema
 
             self.prev_gain_ema = cur_gain_ema 
             self.prev_loss_ema = cur_loss_ema
 
             self.rsi.append(100 - (100 / (1 + cur_gain_ema / cur_loss_ema)))
-    
-    def calculate_rsi(self):
+
+    def update_prices(self):
+        """Appends the most recently closed price to the price array."""
+
+        r = requests.get("https://api.binance.com/api/v1/klines", 
+                        params={
+                            "interval": self.interval, 
+                            "symbol": self.symbol, 
+                            "limit": 2
+                        })
+
+        self.prices.append(float(json.loads(r.text)[0][4]))
+
+        print(self.prices)
+
+    def update_rsi(self):
         """Calculate and append current RSI.
         
         This method should be called once at the start of every new time 
         interval. 
         """
-
         change = self.prices[-1] - self.prices[-2]
 
         if change > 0:
@@ -73,63 +88,66 @@ class Asset:
             u = 0
             d = -change
 
-        cur_gain_ema = a * u + (1 - a) * self.prev_gain_ema
-        cur_loss_ema = a * d + (1 - a) * self.prev_loss_ema
+        cur_gain_ema = self.a * u + (1 - self.a) * self.prev_gain_ema
+        cur_loss_ema = self.a * d + (1 - self.a) * self.prev_loss_ema
 
         self.prev_gain_ema = cur_gain_ema 
         self.prev_loss_ema = cur_loss_ema
         
         self.rsi.append(100 - (100 / (1 + cur_gain_ema / cur_loss_ema)))
-       
-      
-def find_lows(prices, rsi_array):
-    price_lows = []
-    rsi_lows = []
-    # Price lows
-    for i in range(14, len(self.prices)):
+
+
+
+    def find_lows(self, prices, rsi_array):
+        price_lows = []
+        rsi_lows = []
+        # Price lows
+        for i in range(14, len(self.prices)):
+            
+            if i == 0 and self.prices[i] <= self.prices[i + 1]:
+                price_lows.append(self.prices[i]) 
+            elif i == len(self.prices) - 1 and self.prices[i] <= self.prices[i - 1]:
+                price_lows.append(self.prices[i])
+            elif self.prices[i - 1] >= self.prices[i] <= self.prices[i + 1]:
+                price_lows.append(self.prices[i])
+            else:
+                price_lows.append(-1)
         
-        if i == 0 and self.prices[i] <= self.prices[i + 1]:
-            price_lows.append(self.prices[i]) 
-        elif i == len(self.prices) - 1 and self.prices[i] <= self.prices[i - 1]:
-            price_lows.append(self.prices[i])
-        elif self.prices[i - 1] >= self.prices[i] <= self.prices[i + 1]:
-            price_lows.append(self.prices[i])
-        else:
-            price_lows.append(-1)
-    
-    # RSI lows
-    for i in range(len(rsi_array)):
-        if i == 0 and rsi_array[i] <= rsi_array[i + 1]:
-            rsi_lows.append(rsi_array[i]) 
-        elif i == len(rsi_array) - 1 and rsi_array[i] <= rsi_array[i - 1]:
-            rsi_lows.append(rsi_array[i])
-        elif rsi_array[i - 1] >= rsi_array[i] <= rsi_array[i + 1]:
-            rsi_lows.append(rsi_array[i])
-        else:
-            rsi_lows.append(-1)
+        # RSI lows
+        for i in range(len(rsi_array)):
+            if i == 0 and rsi_array[i] <= rsi_array[i + 1]:
+                rsi_lows.append(rsi_array[i]) 
+            elif i == len(rsi_array) - 1 and rsi_array[i] <= rsi_array[i - 1]:
+                rsi_lows.append(rsi_array[i])
+            elif rsi_array[i - 1] >= rsi_array[i] <= rsi_array[i + 1]:
+                rsi_lows.append(rsi_array[i])
+            else:
+                rsi_lows.append(-1)
 
-    return (price_lows, rsi_lows)
+        return (price_lows, rsi_lows)
 
 
-def find_divergence(price_lows, rsi_lows):
-    min_price = math.inf
-    min_rsi = 0
+    def find_divergenceself(self, price_lows, rsi_lows):
+        min_price = math.inf
+        min_rsi = 0
 
-    for i in range(len(price_lows)):
+        for i in range(len(price_lows)):
 
-        if -1 < price_lows[i] < min_price and -1 < rsi_lows[i] < 30:
-            min_price = price_lows[i]
-            min_rsi = rsi_lows[i]
+            if -1 < price_lows[i] < min_price and -1 < rsi_lows[i] < 30:
+                min_price = price_lows[i]
+                min_rsi = rsi_lows[i]
 
-    return (min_price, min_rsi) 
+        return (min_price, min_rsi) 
 
 
 
 
-bitcoin_usdt = Asset("15m", "BTCUSDT")
+bitcoin_usdt = Asset("1m", "BTCUSDT")
 
-print(bitcoin_usdt.closing_prices)
-print(bitcoin_usdt.rsi)
+bitcoin_usdt.update_prices()
+bitcoin_usdt.update_prices()
+bitcoin_usdt.update_prices()
+bitcoin_usdt.update_prices()
 
 """
 lows = find_lows(closing_prices, calculate_rsi(closing_prices))
